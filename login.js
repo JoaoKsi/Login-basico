d7d084cc0c2b4d0c9b6787ab579d1cc802572395
JoaoKsi <joaoksiaskiewcz.1500@hotmail.com>
2025-04-28 16:28:40 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function verificarLogin() {
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+    let mensagemDiv = document.getElementById("mensagem");
+
+    if (usuario === "" || senha === "") {
+        mensagemDiv.textContent = "Preencha todos os campos.";
+        mensagemDiv.className = "mensagem erro";
+    } else if (senha === "12345") {
+        mensagemDiv.textContent = `Bem-vindo, ${usuario}!`;
+        mensagemDiv.className = "mensagem sucesso";
+    } else {
+        mensagemDiv.textContent = "Senha incorreta. Tente novamente.";
+        mensagemDiv.className = "mensagem erro";
+    }
+}
